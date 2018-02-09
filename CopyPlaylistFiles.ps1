85be29c16ba424acc697dc050fd62e774954d9b7
Haikko Tuvi <haikko.tuvi@gmail.com>
2018-02-09 19:07:29 +0200

    Initial

---
+++ CopyPlaylistFiles.ps1
@@ -0,0 +1,9 @@
+﻿# copies all song files in playlist to spetc dir
+$PlaylistFile = "Pidu_Tants.m3u";
+$destUri = "C:\Users\Haikk\Music\Pulmamiks";
+
+Get-Content $PlaylistFile |
+  Select-String '^[^#]' | 
+  ForEach-Object {     
+    Copy-Item -Path $_ -Destination $destUri
+  }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Personal
+For home use
