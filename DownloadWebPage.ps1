1382c79b137f6596a00e0b37d29afff3654fe1b4
haikz <haikko.tuvi@gmail.com>
2018-02-28 15:25:11 +0200

    Add files via upload

---
+++ DownloadWebPage.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-WebPage
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [String]$TestFolder,
+        [Parameter(Mandatory=$true)]
+        [String]$SourcePage
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if (!(Test-Path($TestFolder)))
+        {
+            mkdir $TestFolder
+            Write-Verbose "New folder created";
+        }
+        $web = New-Object System.Net.WebClient
+        $web.DownloadFile($SourcePage, "$TestFolder\index.html")
+    }
+    End
+    {
+    }
+}
+# https://www.autoleht.ee/ostujuht/arvustused
+# Get-WebPage ".\autoleht" "https://www.autoleht.ee/ostujuht/arvustused" -Verbose
